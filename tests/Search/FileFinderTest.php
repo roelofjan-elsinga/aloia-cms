19fdb60fd0b4ef9da7b646cffaa51b757cc4b9c9
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2019-10-09 10:43:46 +0200

    Added file finder tests

---
+++ tests/Search/FileFinderTest.php
@@ -0,0 +1,107 @@
+<?php
+
+
+namespace FlatFileCms\Tests\Search;
+
+use FlatFileCms\Article;
+use FlatFileCms\Search\FileFinder;
+use FlatFileCms\Tests\TestCase;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
+
+class FileFinderTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Using the real filesystem here,
+        // because the native linux command doesn't recognize the virtual filesystem
+
+        mkdir(__DIR__.'/test', 0777, true);
+
+        Config::set('flatfilecms.articles.folder_path', __DIR__.'/test');
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->recursively_remove_directory(__DIR__.'/test');
+    }
+
+    private function recursively_remove_directory($dir)
+    {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir."/".$object) == "dir") {
+                        $this->recursively_remove_directory($dir."/".$object);
+                    } else {
+                        unlink($dir."/".$object);
+                    }
+                }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
+    }
+
+    public function test_no_results_are_returned_when_no_matching_files_found()
+    {
+        $results = FileFinder::find(Article::instance(), 'testing');
+
+        $this->assertCount(0, $results);
+    }
+
+    public function test_only_files_with_matching_content_are_found_for_search_terms()
+    {
+        Article::update(
+            Collection::make([
+                [
+                    'filename' => 'testing.md',
+                    'postDate' => date('Y-m-d'),
+                    'isPublished' => true
+                ],
+                [
+                    'filename' => 'homepage.md',
+                    'postDate' => date('Y-m-d'),
+                    'isPublished' => true
+                ],
+            ])
+        );
+
+        file_put_contents(__DIR__.'/test/testing.md', '# Testing');
+        file_put_contents(__DIR__.'/test/homepage.md', '# Homepage');
+
+        $results = FileFinder::find(Article::instance(), 'testing');
+
+        $this->assertCount(1, $results);
+    }
+
+    public function test_finder_only_returns_published_content()
+    {
+        Article::update(
+            Collection::make([
+                [
+                    'filename' => 'testing.md',
+                    'postDate' => date('Y-m-d'),
+                    'isPublished' => true
+                ],
+                [
+                    'filename' => 'homepage.md',
+                    'postDate' => date('Y-m-d'),
+                    'isPublished' => false
+                ],
+            ])
+        );
+
+        file_put_contents(__DIR__.'/test/testing.md', '# Testing things');
+        file_put_contents(__DIR__.'/test/homepage.md', '# Homepage things');
+
+        $results = FileFinder::find(Article::instance(), 'things');
+
+        $this->assertCount(1, $results);
+    }
+}
