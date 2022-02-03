c51b331ee95d93403325d7f840395421cc3d9670
Roelof Jan <roelof.jan@hey.com>
2022-02-03 20:25:32 +0100

    Added exception handling for creating a folder

--- src/Writer/FolderCreator.php
+++ src/Writer/FolderCreator.php
@@ -9,11 +9,14 @@ class FolderCreator
      * Create a folder for the given path if it doesn't exist
      *
      * @param string $folder_path
+     * @throws \Exception
      */
     public static function forPath(string $folder_path): void
     {
         if (!file_exists($folder_path)) {
-            mkdir($folder_path, 0777, true);
+            if (!mkdir($folder_path, 0777, true)) {
+                throw new \Exception("Failed to create folder for path {$folder_path}");
+            }
         }
     }
 }
