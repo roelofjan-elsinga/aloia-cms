95c5a81fd24cfc3a62509e437ef617bbfb8c0c6c
Roelof Jan <roelof.jan@hey.com>
2022-04-30 12:55:51 +0200

    Added authentication block to config

--- config/aloiacms.php
+++ config/aloiacms.php
@@ -7,25 +7,13 @@ return [
     * */
     'collections_path' => storage_path('app/collections'),
 
-    ],
-
-    'permissions' => [
+    'authentication' => [
 
         /*
-         * This value represents the user the webserver uses to serve the static files
+         * The User model used for authentication
          * */
-        'user' => env('FILE_OWNER', 'www-data'),
+        'model' => \AloiaCms\Auth\User::class
 
-        /*
-         * This value represents the user group the webserver user belongs to
-         * */
-        'group' => env('FILE_GROUP', 'www-data'),
-
-        /*
-         * This value represents any other folder/file path you want to update when setting the file permissions
-         * */
-        'additional_paths' => []
-
-    ],
+    ]
 
 ];
