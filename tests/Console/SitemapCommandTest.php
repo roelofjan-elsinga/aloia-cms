6f1ba6dc7dbf843476e37a02e4c0db457f8f929a
Roelof Jan <roelof.jan@hey.com>
2022-10-26 20:08:55 +0200

    Migrated command tests to pest

--- tests/Console/SitemapCommandTest.php
+++ tests/Console/SitemapCommandTest.php
@@ -1,27 +1,20 @@
 <?php
 
-namespace AloiaCms\Tests\Console;
-
 use AloiaCms\Models\Article;
-use AloiaCms\Tests\TestCase;
 use Illuminate\Support\Facades\Config;
 
-class SitemapCommandTest extends TestCase
-{
-    public function test_sitemap_can_be_generated_through_command()
-    {
-        Article::find('test')
-            ->setPostDate(now())
-            ->save();
+test('sitemap can be generated through the command', function () {
+    Article::find('test')
+        ->setPostDate(now())
+        ->save();
 
-        $this
-            ->artisan('aloiacms:sitemap')
-            ->assertExitCode(0);
+    $this
+        ->artisan('aloiacms:sitemap')
+        ->assertExitCode(0);
 
-        $sitemap_path = Config::get('aloiacms.seo.sitemap_path');
+    $sitemap_path = Config::get('aloiacms.seo.sitemap_path');
 
-        $this->assertFileExists($sitemap_path);
+    $this->assertFileExists($sitemap_path);
 
-        $this->assertTrue(str_contains(file_get_contents($sitemap_path), "http://localhost/articles/test"));
-    }
-}
+    $this->assertTrue(str_contains(file_get_contents($sitemap_path), "http://localhost/articles/test"));
+});
